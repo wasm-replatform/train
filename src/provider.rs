004e0f1bce4dc170535249f466c35a4da52bc8ac
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-10-15 18:33:46 +1300

    outgoing http request provider

--- src/provider.rs
+++ src/provider.rs
@@ -1,63 +1,30 @@
-#![allow(missing_docs)]
+use anyhow::{Context, Result, anyhow};
+use http::{Request, Response};
+use r9k_position::HttpRequest;
+use serde::de::DeserializeOwned;
 
-use std::collections::HashMap;
+pub struct Provider;
 
-use anyhow::{Result, anyhow};
-use r9k_position::gtfs::StopInfo;
-use r9k_position::provider::{Key, Provider, Source, SourceData};
+impl r9k_position::Provider for Provider {}
 
-#[allow(unused_imports)]
-use crate::{BLOCK_MGT_ADDR, GTFS_API_ADDR};
+impl HttpRequest for Provider {
+    async fn fetch<B: Sync, U: DeserializeOwned>(
+        &self, request: &Request<B>,
+    ) -> Result<Response<U>> {
+        tracing::debug!("request: {:?}", request.uri());
 
-#[derive(Debug, Clone, Default)]
-pub struct AppContext {
-    stops: HashMap<&'static str, StopInfo>,
-    vehicles: HashMap<&'static str, String>,
-}
+        let response = sdk_http::Client::new()
+            .get(request.uri())
+            .headers(request.headers())
+            .send()
+            .map_err(|e| anyhow!(e))?;
 
-impl AppContext {
-    pub fn new() -> Self {
-        let stops = HashMap::from([
-            (
-                "133",
-                StopInfo { stop_code: "133".to_string(), stop_lat: -36.12345, stop_lon: 174.12345 },
-            ),
-            (
-                "134",
-                StopInfo { stop_code: "134".to_string(), stop_lat: -36.20299, stop_lon: 174.76915 },
-            ),
-            (
-                "9218",
-                StopInfo { stop_code: "9218".to_string(), stop_lat: -36.567, stop_lon: 174.44444 },
-            ),
-        ]);
-        let vehicles = HashMap::from([("5226", "vehicle 1".to_string())]);
-
-        Self { stops, vehicles }
-    }
-}
-
-impl Provider for AppContext {}
-
-impl Source for AppContext {
-    async fn fetch(&self, _owner: &str, key: &Key) -> Result<SourceData> {
-        match key {
-            // TODO: call GTFS API
-            Key::StopInfo(stop_code) => {
-                let stop_info = self
-                    .stops
-                    .get(stop_code.as_str())
-                    .cloned()
-                    .ok_or_else(|| anyhow!("stop info not found for stop code {stop_code}"))?;
-                Ok(SourceData::StopInfo(stop_info))
-            }
-            // TODO: call Block Management API
-            Key::BlockMgt(train_id) => {
-                let Some(vehicle) = self.vehicles.get(train_id.as_str()) else {
-                    return Ok(SourceData::BlockMgt(vec![]));
-                };
-                Ok(SourceData::BlockMgt(vec![vehicle.clone()]))
-            }
+        let data = response.body();
+        if data.is_empty() {
+            return Err(anyhow!("empty response"));
         }
+
+        let body = serde_json::from_slice::<U>(data).context("deserializing response body")?;
+        Response::builder().status(200).body(body).map_err(|e| anyhow!(e))
     }
 }

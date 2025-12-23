ac92b92d85d4459172c3b0453c037d1b0904817c
Andrew Weston <andrew.weston@propellerhead.co.nz>
2025-12-23 14:21:09 +1300

    update to wrt 0.19.0

--- examples/train.rs
+++ examples/train.rs
@@ -1,10 +1,10 @@
 cfg_if::cfg_if! {
     if #[cfg(not(target_arch = "wasm32"))] {
-        use wasi_http::{WasiHttp, WasiHttpCtxImpl as HttpDefault};
-        use wasi_identity::{WasiIdentity, WasiIdentityCtxImpl as IdentityDefault};
-        use wasi_keyvalue::{WasiKeyValue, WasiKeyValueCtxImpl as KeyValueDefault};
-        use wasi_messaging::{WasiMessaging, WasiMessagingCtxImpl as MessagingDefault};
-        use wasi_otel::{WasiOtel, WasiOtelCtxImpl as OtelDefault};
+        use wasi_http::{WasiHttp, HttpDefault};
+        use wasi_identity::{WasiIdentity,  IdentityDefault};
+        use wasi_keyvalue::{WasiKeyValue, KeyValueDefault};
+        use wasi_messaging::{WasiMessaging, MessagingDefault};
+        use wasi_otel::{WasiOtel,  OtelDefault};
 
         buildgen::runtime!(main, {
             WasiHttp: HttpDefault,
